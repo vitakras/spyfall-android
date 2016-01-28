9358704bd6819f7d9b1d469037d738efcde50f3a
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-01-27 19:57:51 -0500

    Created Location Class

---
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/resources/Location.java
@@ -0,0 +1,29 @@
+package com.vitaliy.krasylovets.spyfall.resources;
+
+import java.util.List;
+
+/**
+ * Created by vitaliy on 2016-01-27.
+ */
+public class Location {
+
+    private String name;
+    private List<Profession> professionList;
+
+    public Location(String name, List<Profession> professionList){
+        this.name = name;
+        this.professionList = professionList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Profession> getProfessionList() {
+        return professionList;
+    }
+}
