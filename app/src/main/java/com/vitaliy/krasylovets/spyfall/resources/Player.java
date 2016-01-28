3c70f6e14dd19ae9d1d3b0de99c807d4cd2706ad
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-01-27 20:02:08 -0500

    created player class

---
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/resources/Player.java
@@ -0,0 +1,35 @@
+package com.vitaliy.krasylovets.spyfall.resources;
+
+/**
+ * Created by vitaliy on 2016-01-27.
+ */
+public class Player {
+
+    private String name;
+    private Profession profession;
+
+    public Player(String name){
+        this.name;
+        this.profession = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public void resetProfession() {
+        this.profession = null;
+    }
+}
