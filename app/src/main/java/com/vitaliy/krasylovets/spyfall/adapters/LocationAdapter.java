0f08600c9502b98a8ac9e28c0477c4e8c11448c9
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-02-13 15:30:06 -0500

    moved LocationViewHolder into LocationAdapter and updated Location Adapter to hide Professions

--- app/src/main/java/com/vitaliy/krasylovets/spyfall/adapters/LocationAdapter.java
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/adapters/LocationAdapter.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.vitaliy.krasylovets.spyfall.LinearListLayout;
 import com.vitaliy.krasylovets.spyfall.R;
 import com.vitaliy.krasylovets.spyfall.resources.Location;
 
@@ -15,17 +17,18 @@ import java.util.List;
 /**
  * Created by vitaliy on 2016-02-09.
  */
-public class LocationAdapter extends RecyclerView.Adapter<LocationViewHolder> {
+public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.LocationViewHolder> {
 
     // Variables
-    List<Location> locationList = Collections.emptyList();
+    private List<Location> locationList = Collections.emptyList();
     private LayoutInflater inflater;
-    private Context context;
+    private OnItemClickListener onItemClickListener;
+    private int selectedLocation;
 
     public LocationAdapter(Context context, List<Location> locationList) {
         this.locationList = locationList;
         this.inflater = LayoutInflater.from(context);
-        this.context = context;
+        this.resetSelectedLocation();
     }
 
     @Override
@@ -37,10 +40,57 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationViewHolder> {
     @Override
     public void onBindViewHolder(LocationViewHolder holder, int position) {
         holder.setLocation(this.locationList.get(position));
+
+        View view = holder.itemView.findViewById(R.id.professionList);
+        if (this.selectedLocation == position){
+            view.setVisibility(View.VISIBLE);
+        } else {
+            view.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public int getItemCount() {
         return this.locationList.size();
+    }
+
+    public void resetSelectedLocation() {
+        this.selectedLocation = -1;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public class LocationViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private TextView textView;
+        private LinearListLayout linearListLayout;
+        private Context context;
+
+        public LocationViewHolder(View itemView) {
+            super(itemView);
+
+            this.context = itemView.getContext();
+            this.textView = (TextView) itemView.findViewById(R.id.title);
+            this.linearListLayout = new LinearListLayout((ViewGroup) itemView.findViewById(R.id.professionList));
+        }
+
+        public void setLocation(Location location){
+            this.textView.setText(location.getName());
+            this.linearListLayout.setAdapter(new ProfessionAdapter(context,
+                    location.getProfessionList()));
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
