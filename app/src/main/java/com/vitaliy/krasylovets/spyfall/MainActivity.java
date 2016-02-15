717f5c781305e6b4c7147e168feaf8198f83081e
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-02-15 12:50:16 -0500

    updated main activity to exit app and Start new Game activity

--- app/src/main/java/com/vitaliy/krasylovets/spyfall/MainActivity.java
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/MainActivity.java
@@ -1,29 +1,43 @@
 package com.vitaliy.krasylovets.spyfall;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * MainActivity Responsible for Selecting Different game modes,
+ * options and being able to exit the game
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setupOnClickListeners();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.local_play_btn:
+                Intent intent = new Intent(this, NewGameActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.exit_btn:
+                finish();
+                break;
+        }
+    }
+
+    private void setupOnClickListeners() {
+        findViewById(R.id.local_play_btn).setOnClickListener(this);
+        findViewById(R.id.exit_btn).setOnClickListener(this);
+    }
 }
