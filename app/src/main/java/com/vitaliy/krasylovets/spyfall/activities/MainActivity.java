f53582b729e9aaeb60c317edf39b346bd769031a
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-02-20 10:52:51 -0500

    removed unused activities

--- app/src/main/java/com/vitaliy/krasylovets/spyfall/activities/MainActivity.java
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/activities/MainActivity.java
@@ -1,46 +1,71 @@
 package com.vitaliy.krasylovets.spyfall.activities;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
-import com.vitaliy.krasylovets.spyfall.NewGameActivity;
 import com.vitaliy.krasylovets.spyfall.R;
+import com.vitaliy.krasylovets.spyfall.Utils;
+import com.vitaliy.krasylovets.spyfall.fragments.NewGameFragment;
+import com.vitaliy.krasylovets.spyfall.resources.Location;
+import com.vitaliy.krasylovets.spyfall.resources.Player;
+import com.vitaliy.krasylovets.spyfall.resources.SpyFall;
+import com.vitaliy.krasylovets.spyfall.resources.SpyFallFactory;
+import com.vitaliy.krasylovets.spyfall.resources.SpyProfession;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
- * MainActivity Responsible for Selecting Different game modes,
- * options and being able to exit the game
+ * Created by vitaliy on 2016-02-15.
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements NewGameFragment.OnNewGameListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupOnClickListeners();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("SpyFall");
+
+        FragmentManager fm = getSupportFragmentManager();
+        NewGameFragment ngf = NewGameFragment.newInstance();
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragment_container, ngf);
+        ft.commit();
     }
 
+    /**
+     * NewGameFragment OnNewGameListener
+     * @param playerList players for the new game
+     */
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.local_play_btn:
-                Intent intent = new Intent(this, LocalPlayActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.exit_btn:
-                finish();
-                break;
-        }
-    }
+    public void onNewGame(List<Player> playerList) {
+        InputStream stream = getResources().openRawResource(R.raw.locations);//Resources.getSystem().openRawResource(R.raw.locations);
 
-    private void setupOnClickListeners() {
-        findViewById(R.id.local_play_btn).setOnClickListener(this);
-        findViewById(R.id.exit_btn).setOnClickListener(this);
+        List<Location> locations = null;
+        try {
+            locations = SpyFallFactory.getLocationList(Utils.readResourceData(stream));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SpyFall game = new SpyFall(playerList,locations, new SpyProfession("Spy"));
+        game.newGame();
+
+        for (Player player : playerList){
+            Log.d("Player", "onNewGame: " + player.toString());
+        }
     }
 }
