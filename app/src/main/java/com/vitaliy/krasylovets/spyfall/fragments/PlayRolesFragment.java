5131854e25f1f5726b92bf7b3bb67e8a45b1cdc5
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-02-22 20:22:59 -0500

    created Player Roles Fragment

---
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/fragments/PlayRolesFragment.java
@@ -0,0 +1,10 @@
+package com.vitaliy.krasylovets.spyfall.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * This Fragment is responsible for displaying player roles
+ * Created by vitaliy on 2016-02-22.
+ */
+public class PlayRolesFragment extends Fragment {
+}
