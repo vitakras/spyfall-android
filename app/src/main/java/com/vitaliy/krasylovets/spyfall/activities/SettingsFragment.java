7ac331516f2635278069ec73bdcf1339a0576002
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-03-12 09:01:39 -0500

    created new Settings activity with settings fragment

---
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/activities/SettingsFragment.java
@@ -0,0 +1,24 @@
+package com.vitaliy.krasylovets.spyfall.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vitaliy.krasylovets.spyfall.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
