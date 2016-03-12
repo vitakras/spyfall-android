0e36e1b5424aa6308eba958234df25851c5bf8c7
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-03-12 13:32:50 -0500

    fixed recycle view issue after updating grade and removed support libraries

--- app/src/main/java/com/vitaliy/krasylovets/spyfall/activities/MainActivity.java
+++ app/src/main/java/com/vitaliy/krasylovets/spyfall/activities/MainActivity.java
@@ -1,15 +1,8 @@
 package com.vitaliy.krasylovets.spyfall.activities;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
